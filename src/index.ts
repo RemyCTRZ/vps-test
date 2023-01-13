ddecc72b2e74c849f43268fb6daeb74cc7992cde
Rémy <104489124+RemyCTRZ@users.noreply.github.com>
2023-01-13 14:13:27 +0100

    test modif

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ require("dotenv")
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Rémy 1'));
+app.get('/hello', (req, res) => res.send('Hello Rémy'));
 app.get('/docker', (req, res) => res.send("J'aime " + process.env.test));
 
 app.listen(3010, () => console.log("Application démarrée sur le port 3010"));
