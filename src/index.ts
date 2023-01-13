bbffb1f1d96c29b333271a7cac893b58eb36293f
Rémy <104489124+RemyCTRZ@users.noreply.github.com>
2023-01-13 13:58:59 +0100

    normalement ça marche

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+require("dotenv")
 
 const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello Rémy 1'));
+app.get('/docker', (req, res) => res.send("J'aime " + process.env.test));
 
-app.listen(3010, ()=> console.log("Application démarrée sur le port 3010"));
+app.listen(3010, () => console.log("Application démarrée sur le port 3010"));
